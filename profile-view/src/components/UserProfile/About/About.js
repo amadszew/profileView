0dd35cec780a95a2994117cc2e814f7599a68999
amadeuszszewczyk <43135476+srubububudu@users.noreply.github.com>
2019-12-09 15:57:03 +0100

    css

--- profile-view/src/components/UserProfile/About/About.js
+++ profile-view/src/components/UserProfile/About/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './About.scss';
 
 class About extends Component {
 
@@ -10,28 +11,30 @@ class About extends Component {
         const profileList = profiles.map((param, index) => {
             return (
                 <div key={index}>
-                    <header className="about__header">
-                        <h1>{param.userName}</h1>
-                        <div> 
+                    <header className="about-header">
+                        <h1 className="about-header__name">{param.userName}</h1>
+                        <div className="about-header__address"> 
                             <FontAwesomeIcon
                                 icon="map-marker-alt"
+                                color="#828282"
+                                size="md"
                             />
                             <p>
                                 {param.city}, {param.country}
                             </p>
                         </div>
                     </header>
-                    <div className="about__text">
-                        <p>{param.firstText}</p>
-                        <blockquote>{param.quoteText}</blockquote>
-                        <p>{param.secondText}</p>
+                    <div className="about-text">
+                        <p className="about-text__first">{param.firstText}</p>
+                        <blockquote className="about-text__quote">{param.quoteText}</blockquote>
+                        <p className="about-text__second">{param.secondText}</p>
                     </div>
                 </div>
             )
         })
         
         return ( 
-            <section className="about__container">
+            <section className="about-container">
                 {profileList}
             </section>
         );
