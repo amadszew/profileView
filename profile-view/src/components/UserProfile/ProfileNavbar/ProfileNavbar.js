336affb1d041a1bad63e5f19fcf5e2f2c77cd065
amadeuszszewczyk <43135476+srubububudu@users.noreply.github.com>
2019-12-09 13:30:53 +0100

    user profile and navbar views, basic css added

--- profile-view/src/components/UserProfile/ProfileNavbar/ProfileNavbar.js
+++ profile-view/src/components/UserProfile/ProfileNavbar/ProfileNavbar.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import sand from '../../../assets/sand-bg.png';
+import avatar from '../../../assets/avatar.png';
+import './ProfileNavbar.scss';
 
 export const ProfileNavbar = () => {
     return (
-        <div className="profile-navbar">
-
+        <div className="profile-navbar__container">
+            <img className="profile-navbar__background" src={sand} alt="sandy background" /> 
+            <nav className="profile-navbar__nav" >
+                <img className="profile-navbar__avatar" src={avatar} alt="avatar" />
+                <li>
+                    <NavLink exact to={`/`}>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink exact to={`/gallery`}>
+                        Gallery
+                    </NavLink>
+                </li>
+            </nav>
         </div>
     );
 }
--- profile-view/src/components/UserProfile/UserProfile.js
+++ profile-view/src/components/UserProfile/UserProfile.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProfileNavbar from './ProfileNavbar/ProfileNavbar';
+import About from './About/About';
+import Gallery from './Gallery/Gallery';
+import './UserProfile.scss';
 
 const UserProfile = () => {
 
     return ( 
-        <div className="user-profile__container">
-            
-        </div>
+        <Router>
+            <div className="user-profile__container">
+                <ProfileNavbar />
+                <div className="user-profile__displayer">
+                    <Route exact path="/">
+                        <About />
+                    </Route>
+                    <Route path="/gallery">
+                        <Gallery />
+                    </Route>
+                </div>
+            </div>
+        </Router>
     );
 }
 export default UserProfile;
