08a71ffe04e55bfa00a02d3d307b90386b263a88
amadeuszszewczyk <43135476+srubububudu@users.noreply.github.com>
2019-12-09 22:05:49 +0100

    move componentDidMount and state from Gallery to UserProfile component, view fixes

--- profile-view/src/components/UserProfile/Gallery/Gallery.js
+++ profile-view/src/components/UserProfile/Gallery/Gallery.js
@@ -1,44 +1,13 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './Gallery.scss'
 
-class Gallery extends Component {
-    constructor() {
-        super();
-        this.state = {
-            pictures: []
-        };
-    }
-
-    componentDidMount() {
-        fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1d3de119cec5023167d5b9218d0ac0ba&tags=NormaJeaneMortenson&per_page=9&page=1&format=json&nojsoncallback=1")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(j) {
-            let picArray = j.photos.photo.map((pic, index) => {
-                const srcPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
-                
-                console.log(srcPath)
-                return (
-                     <div key={index} className="gallery-box">
-                        <a href={srcPath} target="_blank" className="gallery-box__picture">
-                            <img alt='marilyn' src={srcPath} />
-                        </a>
-                     </div>
-                )
-            })
-            this.setState({pictures: picArray});
-        }.bind(this))
-    }
+const Gallery = (props) => {
     
-    render() {
-        return ( 
-            <div className="gallery-container">
-                {this.state.pictures}
-            </div>
-        );
-    }
+    return ( 
+        <div className="gallery-container">
+            {props.passPictures}
+        </div>
+    );
 }
 
 export default Gallery;
--- profile-view/src/components/UserProfile/UserProfile.js
+++ profile-view/src/components/UserProfile/UserProfile.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProfileNavbar from './ProfileNavbar/ProfileNavbar';
 import About from './About/About';
 import Gallery from './Gallery/Gallery';
 import './UserProfile.scss';
 
-const UserProfile = () => {
+class UserProfile extends Component  {
+    constructor() {
+        super();
+        this.state = {
+            pictures: []
+        };
+    } 
 
-    return ( 
-        <Router>
-            <div className="user-profile__container">
-                <ProfileNavbar />
-                <div className="user-profile__displayer">
-                    <Route exact path="/">
-                        <About />
-                    </Route>
-                    <Route path="/gallery">
-                        <Gallery />
-                    </Route>
+    //Download gallery from Flickr for Gallery component
+    componentDidMount() {
+        fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1d3de119cec5023167d5b9218d0ac0ba&tags=NormaJeaneMortenson&per_page=9&page=1&format=json&nojsoncallback=1")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(j) {
+            let picArray = j.photos.photo.map((pic, index) => {
+                const srcPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
+                
+                console.log(srcPath)
+                return (
+                     <div key={index} className="gallery-box">
+                        <a href={srcPath} target="_blank" className="gallery-box__picture">
+                            <img alt='marilyn' src={srcPath} />
+                        </a>
+                     </div>
+                )
+            })
+            this.setState({pictures: picArray});
+        }.bind(this))
+    }
+
+    render() {
+        return ( 
+            <Router>
+                <div className="user-profile__container">
+                    <ProfileNavbar />
+                    <div className="user-profile__displayer">
+                        <Route exact path="/">
+                            <About />
+                        </Route>
+                        <Route path="/gallery"
+                        render={(props) => <Gallery {...props}
+                            passPictures={this.state.pictures} />}
+                        />
+                    </div>
                 </div>
-            </div>
-        </Router>
-    );
+            </Router>
+        );
+    }
 }
 export default UserProfile;
                 
